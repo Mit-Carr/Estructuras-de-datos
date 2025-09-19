b50c07c5b6562b3ee9aa49995ab7631db6a80ad4
Mitchel Carrillo Uicab <mitchelcarrillo26@gmail.com>
2025-09-18 21:04:25 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -20,4 +20,23 @@ def fibonacci(n):
             b = c
         return b
 
+
 print(fibonacci(18))
+
+def fibonacci_recursivo(n):
+  # Caso base 1: si n es 0, el resultado es 0.
+  if n == 0:
+    return 0
+  
+  # Caso base 2: si n es 1, el resultado es 1.
+  elif n == 1:
+    return 1
+    
+  # Caso recursivo: para cualquier otro número,
+  # es la suma de los dos números de Fibonacci anteriores.
+  else:
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+# --- Pruebas ---
+print(f"Fibonacci de 9 es: {fibonacci_recursivo(9)}")   # Salida: 34
+print(f"Fibonacci de 18 es: {fibonacci_recursivo(18)}") # Salida: 2584
